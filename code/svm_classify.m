374fd559ee587a8c3d9dfd813241011664fcf19e
ScottG <52426901+scott-gurney@users.noreply.github.com>
2024-04-22 22:56:48 +0100

    code for SVM classifier

--- code/svm_classify.m
+++ code/svm_classify.m
@@ -18,32 +18,43 @@ function predicted_categories = svm_classify(train_image_feats, train_labels, te
 % predicted_categories is an M x 1 cell array, where each entry is a string
 %  indicating the predicted category for each test image.
 
-%{
-Useful functions:
- matching_indices = strcmp(string, cell_array_of_strings)
- 
-  This can tell you which indices in train_labels match a particular
-  category. This is useful for creating the binary labels for each SVM
-  training task.
 
-[W B] = vl_svmtrain(features, labels, LAMBDA)
-  http://www.vlfeat.org/matlab/vl_svmtrain.html
+% Create unique list of categories
+categories=unique(train_labels);
 
-  This function trains linear svms based on training examples, binary
-  labels (-1 or 1), and LAMBDA which regularizes the linear classifier
-  by encouraging W to be of small magnitude. LAMBDA is a very important
-  parameter! You might need to experiment with a wide range of values for
-  LAMBDA, e.g. 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10.
+% Number of unique categories
+num_categories=length(categories);
 
-  Matlab has a built in SVM, see 'help svmtrain', which is more general,
-  but it obfuscates the learned SVM parameters in the case of the linear
-  model. This makes it hard to compute "confidences" which are needed for
-  one-vs-all classification.
+% Empty cell for each categories model
+SVMModels=cell(num_categories,1);
 
-%}
+% Train for each category
+for idx = 1:num_categories
+    % Create temporary binary class labels
+    temp_labels=strcmp(train_labels,categories(idx)); % Create binary classes for each classifier
+    % Save model by category index
+    SVMModels{idx}=fitcsvm(train_image_feats,temp_labels,'Standardize',true,...
+        'KernelFunction','linear');
+end
 
-%unique() is used to get the category list from the observed training
-%category list. 'categories' will not be in the same order as in coursework_starter,
-%because unique() sorts them. This shouldn't really matter, though.
-categories = unique(train_labels); 
-num_categories = length(categories);
+% Size of test set
+num_test=size(test_image_feats,1);
+
+% Default predicted scores (number of tests x number of categories)
+Scores=zeros(num_test,num_categories);
+
+% Test for each category
+for idx=1:num_categories
+    % Predict and record score only
+    [~,score]=predict(SVMModels{idx},test_image_feats);
+
+    % Save score by category index
+    % Second column contains positive-class scores
+    Scores(:,idx)=score(:,2);
+end
+
+% Find the maximum score and record the indice
+[~,indices]=max(Scores,[],2);
+
+% Match the indices to the corosponding category index
+predicted_categories=categories(indices);
